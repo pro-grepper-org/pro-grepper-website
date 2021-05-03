d99fb9b07fc4027b0e7cd5c7372b72270edf64e9
Chinmay Desai <chinmaycm1@gmail.com>
2021-05-03 17:14:06 +0530

    jwt auth complete, grep API integration done

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const express = require('express')
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv')
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+const connectDB = require('./config/db')
 
-const app = express()
-const port = process.env.PORT || 3000;
-
-app.get('/',(request,response)=>{
-    response.sendFile('./views/landing.html', { root: __dirname });
+dotenv.config({
+    path: "./config/config.env"
 })
 
+// database connection
+connectDB()
 
-app.listen(port , ()=>{
-    console.log('server is running on port '+port);
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/grep', requireAuth, (req, res) => res.render('grep'));
+app.get('/aboutus', (req, res) => res.render('aboutus'));
+app.get('/features', (req, res) => res.render('features'));
+app.use(authRoutes);
+
+
+app.listen(port, () => {
+    console.log('server is running on port ' + port);
 })
