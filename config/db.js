0c0a73d9c98a0776a7399d98703ae77ee4969e28
Aditya Ambekar <adityaambekar.vjti@gmail.com>
2021-05-04 12:27:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://admin:grepper007@progrepperuserdb.irues.mongodb.net/progrepperusers?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
